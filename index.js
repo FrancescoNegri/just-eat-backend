bcdef71e2531658cb6f73747606194ee74940086
FrancescoNegri <francesco.negri@outlook.com>
2017-09-01 21:37:01 +0200

    Update dei ristoranti disponibili ogni 2 minuti

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var restaurantsscraper = require("./restaurants-scraper.js");
 var menuScraper = require("./menu-scraper.js");
 
 //ATOMIC COUNTER VA SCHIFO, pensare ad altre soluzioni
-const firstScraping = () => {
+const completeScraping = () => {
     return new Promise((resolve, reject) => {
         restaurantsscraper.scrapeRestaurants()
             .then(restaurants => {
@@ -24,12 +24,39 @@ const firstScraping = () => {
     });
 }
 
+const scrapingCallback = (restaurants) => {
+    // console.log(JSON.stringify(restaurants));
+    // console.log("\n");
+    console.log(Object.keys(restaurants).length.toString(), "ristoranti disponibili al momento");
 
-firstScraping()
-    .then(restaurants => {
-        console.log(Object.keys(restaurants).length.toString(), "ristoranti disponibili");
-    })
-    .catch(err => {
-        console.log(err);
-    });
+    var updateInterval = setInterval(() => {
+        restaurantsscraper.scrapeRestaurants()
+            .then(newRestaurants => {
+                aKey = JSON.stringify(Object.keys(restaurants).sort());
+                bKey = JSON.stringify(Object.keys(newRestaurants).sort());
+                if (aKey !== bKey) {
+                    console.log("DIVERSO!");
+                    clearInterval(updateInterval);
+                    restaurants = newRestaurants;
+                    completeScraping()
+                        .then(restaurants => {
+                            scrapingCallback(restaurants);
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        });
+                }
+                else {
+                    //console.log("Business as usual");
+                }
+            })
+    }, 2 * 60 * 1000);
+}
 
+completeScraping()
+.then(restaurants => {
+    scrapingCallback(restaurants);
+})
+.catch(err => {
+    console.log(err);
+});
